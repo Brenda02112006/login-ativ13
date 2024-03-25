50bbf09d13dd6c8e9ec2f14dff902bfd98110ab6
Brenda02112006 <139500962+Brenda02112006@users.noreply.github.com>
2024-03-25 10:07:10 -0300

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,13 +1,17 @@
-const { renderHomePage, searchBooks } = require('./controllers');
+const express = require('express');
+const controllers = require('./controllers');
+const middlewares = require('./middlewares');
+const router = express.Router();
 
-module.exports = function (app) {
-  // Rota para a página inicial
-  app.get('/', (req, res) => {
-    renderHomePage(req, res); // Aqui está ocorrendo o erro
-  });
+router.get('/perfil/:email', middlewares.verificarAutenticacao, controllers.renderProfile);
+router.get('/usuarios', middlewares.verificarAutenticacao, controllers.renderUsers);
+router.post('/excluir/:email', middlewares.verificarAutenticacao, controllers.deleteUser);
+router.get('/register', controllers.renderRegister);
+router.post('/register', controllers.registerUser);
+router.get('/', controllers.renderLogin);
+router.post('/login', controllers.loginUser);
+router.get('/logout', controllers.logoutUser);
 
-  // Rota para realizar buscas
-  app.get('/search', (req, res) => {
-    searchBooks(req, res);
-  });
+module.exports = app => {
+    app.use('/', router);
 };
