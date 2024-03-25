34a34c86631b083356c271a951aa6a6c856a1c63
Brenda02112006 <139500962+Brenda02112006@users.noreply.github.com>
2024-03-25 10:06:18 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const routes = require('./routes');
 const app = express();
 const port = 3000;
 
-const routes = require('./routes');
-const { setup } = require('./middleware');
-const { renderHomePage } = require('./controllers');
-
 app.set('view engine', 'ejs');
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+    secret: 'chave-secreta',
+    resave: false,
+    saveUninitialized: true
+}));
 
-setup(app);
 routes(app);
 
 app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
+    console.log(`Servidor está rodando em http://localhost:${port}`);
 });
