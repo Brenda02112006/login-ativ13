16d09b42ea197cd620801ee34e9d07b1a18a314b
Brenda02112006 <139500962+Brenda02112006@users.noreply.github.com>
2024-03-25 14:31:03 -0300

    Update controllers.js

--- controllers.js
+++ controllers.js
@@ -1,25 +1,27 @@
+//exporta as funções
+
 const bcrypt = require('bcrypt');
 
 // Array vazio para armazenar os usuários
 let users = [];
 
 module.exports = {
-    renderProfile: (req, res) => {
-        const { email } = req.params;
+    renderProfile: (req, res) => { //rederiza o perfil de um usuário
+        const { email } = req.params; //extrai o email
         res.render('perfil', { email, users });
     },
-    renderUsers: (req, res) => {
+    renderUsers: (req, res) => { //rederiza a lista de usuários
         res.render('usuarios', { users });
     },
-    deleteUser: (req, res) => {
+    deleteUser: (req, res) => { //remove o usuário
         const email = req.params.email;
         const index = users.findIndex(user => user.email === email);
         if (index !== -1) {
-            users.splice(index, 1);
+            users.splice(index, 1); //remove o usuário da lista
         }
         res.redirect('/usuarios');
     },
-    renderRegister: (req, res) => {
+    renderRegister: (req, res) => { //registra um novo usuario
         res.render('registro', { errorMessage: '' });
     },
     registerUser: (req, res) => {
@@ -53,10 +55,10 @@ module.exports = {
             res.redirect(`/perfil/${newUser.email}`);
         });
     },
-    renderLogin: (req, res) => {
+    renderLogin: (req, res) => { //rederiza o formulario
         res.render('index', { errorMessage: '' });
     },
-    loginUser: (req, res) => {
+    loginUser: (req, res) => { // realiza o login
         const { email, password } = req.body;
         const user = users.find(user => user.email === email);
         if (!user) {
@@ -76,7 +78,7 @@ module.exports = {
             }
         });
     },
-    logoutUser: (req, res) => {
+    logoutUser: (req, res) => { // o logout do usuario
         req.session.destroy(err => {
             if (err) {
                 console.error('Erro ao fazer logout:', err);
