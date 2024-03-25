092610995e923780144386d1869533d6238c6934
Brenda02112006 <139500962+Brenda02112006@users.noreply.github.com>
2024-03-25 14:31:14 -0300

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,3 +1,6 @@
+//exporta verificarAutentificacao
+// se tiver autenticado encaminha para o proximo middelewares
+
 module.exports = {
     verificarAutenticacao: (req, res, next) => {
         if (req.session.usuario) {
