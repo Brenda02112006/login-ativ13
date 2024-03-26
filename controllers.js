e003ab42132ed939006a8bbd0b905e85520beb39
Brenda02112006 <139500962+Brenda02112006@users.noreply.github.com>
2024-03-26 15:19:56 -0300

    Update controllers.js

--- controllers.js
+++ controllers.js
@@ -1,6 +1,3 @@
-//exporta as funções
-const bcrypt = require('bcrypt');
-
 // Array vazio para armazenar os usuários
 let users = [];
 
@@ -37,23 +34,6 @@ const userController = {
         if (existingUser) {
             return res.render('registro', { errorMessage: 'Este email já está cadastrado' });
         }
-
-        // Hash da senha antes de salvar no banco de dados
-        bcrypt.hash(password, 10, (err, hashedPassword) => {
-            if (err) {
-                console.error('Erro ao criar hash da senha:', err);
-                return res.render('registro', { errorMessage: 'Erro ao registrar o usuário' });
-            }
-
-            // Cria um novo usuário com a senha criptografada
-            const newUser = { name, email, password: hashedPassword };
-            // Adiciona o novo usuário à lista de usuários
-            users.push(newUser);
-            // Define o usuário na sessão
-            req.session.usuario = newUser;
-            // Redireciona para a página de perfil do usuário
-            res.redirect(`/perfil/${newUser.email}`);
-        });
     },
     renderLogin: (req, res) => { //rederiza o formulario
         res.render('index', { errorMessage: '' });
@@ -65,18 +45,6 @@ const userController = {
             return res.render('index', { errorMessage: 'Email ou senha incorretos' });
         }
 
-        bcrypt.compare(password, user.password, (err, result) => {
-            if (err) {
-                console.error('Erro ao comparar senhas:', err);
-                return res.render('index', { errorMessage: 'Erro ao fazer login' });
-            }
-            if (result) {
-                req.session.usuario = user;
-                res.redirect(`/perfil/${user.email}`);
-            } else {
-                res.render('index', { errorMessage: 'Email ou senha incorretos' });
-            }
-        });
     },
     logoutUser: (req, res) => { // o logout do usuario
         req.session.destroy(err => {
