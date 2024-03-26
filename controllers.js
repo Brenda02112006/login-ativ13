632e632d18fd70609d7bf8eeed4e8044b13d6bba
Brenda02112006 <139500962+Brenda02112006@users.noreply.github.com>
2024-03-26 15:30:05 -0300

    Update controllers.js

--- controllers.js
+++ controllers.js
@@ -41,34 +41,11 @@ const userController = {
         // Adiciona o novo usuário à lista de usuários
         const newUser = { name, email, password };
         users.push(newUser);
-        // Define o usuário na sessão
-        req.session.usuario = newUser;
         // Redireciona para a página de perfil do usuário
         res.redirect(`/perfil/${newUser.email}`);
     },
     renderLogin: (req, res) => { // Renderiza o formulário de login
         res.render('index', { errorMessage: '' });
-    },
-    loginUser: (req, res) => { // Realiza o login
-        const { email, password } = req.body;
-        const user = users.find(user => user.email === email);
-        if (!user) {
-            return res.render('index', { errorMessage: 'Email ou senha incorretos' });
-        }
-
-        bcrypt.compare(password, user.password, (err, result) => {
-            if (err) {
-                console.error('Erro ao comparar senhas:', err);
-                return res.render('index', { errorMessage: 'Erro ao fazer login' });
-            }
-            if (result) {
-                // Define o usuário na sessão após o login bem-sucedido
-                req.session.usuario = user;
-                res.redirect(`/perfil/${user.email}`);
-            } else {
-                res.render('index', { errorMessage: 'Email ou senha incorretos' });
-            }
-        });
     },
     logoutUser: (req, res) => { // Realiza o logout do usuário
         req.session.destroy(err => {
